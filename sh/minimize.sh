ce2372aaff144508ed26c6904cf38d100749c6bd
Robert Ferney <rob@ferney.org>
2018-02-07 20:45:18 -0700

    update scripts

--- sh/minimize.sh
+++ sh/minimize.sh
@@ -1,23 +1,19 @@
 #!/bin/sh -eux
 
-case "$PACKER_BUILDER_TYPE" in
-  qemu) exit 0 ;;
-esac
-
 # Whiteout root
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
 count=$(($count-1))
-dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
+dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed"
 rm /tmp/whitespace
 
 # Whiteout /boot
 count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
 count=$(($count-1))
-dd if=/dev/zero of=/boot/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
+dd if=/dev/zero of=/boot/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed"
 rm /boot/whitespace
 
 set +e
-swapuuid="`/sbin/blkid -o value -l -s UUID -t TYPE=swap`";
+swapuuid="`/sbin/blkid -o value -l -s UUID -t TYPE=swap`"
 case "$?" in
     2|0) ;;
     *) exit 1 ;;
@@ -33,4 +29,5 @@ if [ "x${swapuuid}" != "x" ]; then
     /sbin/mkswap -U "$swapuuid" "$swappart";
 fi
 
-sync;
+sync
+
--- sh/vagrant.sh
+++ sh/vagrant.sh
@@ -5,6 +5,7 @@ HOME_DIR="${HOME_DIR:-/home/vagrant}";
 
 pubkey_url="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub";
 mkdir -p $HOME_DIR/.ssh;
+
 if command -v wget >/dev/null 2>&1; then
     wget --no-check-certificate "$pubkey_url" -O $HOME_DIR/.ssh/authorized_keys;
 elif command -v curl >/dev/null 2>&1; then
@@ -15,5 +16,7 @@ else
     echo "Cannot download vagrant public key";
     exit 1;
 fi
+
 chown -R vagrant $HOME_DIR/.ssh;
 chmod -R go-rwsx $HOME_DIR/.ssh;
+
