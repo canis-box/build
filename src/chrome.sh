ae2f3b39469906c25e7ce0a9c72fe8af9a76cfe2
Robert Ferney <rob@ferney.org>
2018-02-23 12:50:10 -0700

    add webkit

--- src/chrome.sh
+++ src/chrome.sh
@@ -7,7 +7,7 @@ deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 CFG
 
 apt-get update
-apt-get install -y --allow-unauthenticated \
+apt-get install -y --allow-unauthenticated --no-install-recommends \
   google-chrome-stable chromium-browser chromium-chromedriver
 
 sed -i 's/^[!#]/# /' /etc/apt/sources.list.d/google-chrome.list
